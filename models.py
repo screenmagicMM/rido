4630fad8cb766f80163b09150d8a44b977617596
Tamilselvan R <prtamil@gmail.com>
2016-11-28 23:59:07 +0530

    Adding Variables into SqliteDB from POST Request

--- models.py
+++ models.py
@@ -1,12 +1,12 @@
 import dateutil.parser
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
         
 
-class Task(Base):
+class SimulationTask(Base):
     """
         Takes Tasks so that requsts can work async
         We can track the status
@@ -16,15 +16,49 @@ class Task(Base):
     """
     __tablename__ = 'tasks'
     id = Column(Integer, primary_key=True)
-    name = Column(String(50))
     status = Column(String(50))
+    total_investment = Column(Float)
+    no_of_days = Column(Integer)
+    areas_no = Column(Integer)
+    no_of_kms = Column(Integer)
+    petrol_price = Column(Float)
+    disel_price = Column(Float)
+    lpg_price = Column(Float)
+    no_of_drivers = Column(Integer)
+    drive_time_per_drver = Column(Float)
+    lpg_cars_no = Column(Integer)
+    petrol_cars_no = Column(Integer)
+    disel_cars_no = Column(Integer)
+    avg_maint_cost = Column(Float)
+    avg_petrol_milage = Column(Float)
+    avg_disel_milege = Column(Float)
+    avg_lpg_milege = Column(Float)
 
-    def __init__(self, name=None):
-        self.name = name
+
+    def __init__(self,kwargs):
+        print('init')
+        print(kwargs)
         self.status = 'NOTSTART'
+        self.total_investment = int(kwargs['total_investment'])
+        self.no_of_days = int(kwargs['no_of_days'])
+        self.areas_no = int(kwargs['areas_no'])
+        self.no_of_kms = int(kwargs['no_of_kms'])
+        self.petrol_price = float(kwargs['petrol_price'])
+        self.disel_price = float(kwargs['disel_price'])
+        self.lpg_price = float(kwargs['lpg_price'])
+        self.no_of_drivers = int(kwargs['no_of_drivers'])
+        self.drive_time_per_drver = float(kwargs['drive_time_per_drver'])
+        self.lpg_cars_no = int(kwargs['lpg_cars_no'])
+        self.petrol_cars_no = int(kwargs['petrol_cars_no'])
+        self.disel_cars_no = int(kwargs['disel_cars_no'])
+        self.avg_maint_cost = float(kwargs['avg_maint_cost'])
+        self.avg_petrol_milage = float(kwargs['avg_petrol_milage'])
+        self.avg_disel_milege = float(kwargs['avg_disel_milege'])
+        self.avg_lpg_milege = float(kwargs['avg_lpg_milege'])
+
 
     def __repr__(self):
-        return '<Task %r>' % (self.name)
+        return '<SimulationTask %r>' % (self.id)
 
 class AdminUser(object):
     """
--- routes.py
+++ routes.py
@@ -1,28 +1,44 @@
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response
+from flask import jsonify
 from flask_adminlte import AdminLTE
-from models import AdminUser, Task, Base
-from flask_sqlalchemy import SQLAlchemy
+from models import AdminUser, SimulationTask, Base
+from time import sleep
+from concurrent.futures import ThreadPoolExecutor
+import sys
+import json
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('sqlite:///test.db', convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
+Base.query = db_session.query_property()
+
+    
 
 
 def create_rido_application(configfile=None):
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-    db = SQLAlchemy(app)
     AdminLTE(app)
 
     current_user = AdminUser()
+
+
 
     @app.before_first_request
     def setup():
         print ("First Executed")
         # Recreate database each time for demo
-        Base.metadata.drop_all(bind=db.engine)
-        Base.metadata.create_all(bind=db.engine)
-        db.session.add(Task('First'))
-        db.session.add(Task('Second'))
-        db.session.commit()
+        Base.metadata.drop_all(bind=engine)
+        Base.metadata.create_all(bind=engine)
+        #db.session.add(SimulationTask('First'))
+        #db.session.add(SimulationTask('Second'))
+        db_session.commit()
+        print ('comited happened')
 
     @app.route('/')
     def index():
@@ -43,15 +59,45 @@ def create_rido_application(configfile=None):
     @app.route('/simulation', methods=['POST'])
     def simulation():
         if request.method == 'POST':
-            data = request.form # a multidict containing POST data
-            print(data)
-            
-        return u"<br> Success </br>"    
+            try:
+                data = request.form # a multidict containing POST data
+                print("Simulation Task started!")
+                simdata = {
+                    "total_investment" : data['total_investment'].split(',')[0],
+                    "no_of_days" :data['no_of_days'].split(',')[0],
+                    "areas_no" : data['areas_no'].split(',')[0],
+                    "no_of_kms" : data['no_of_kms'].split(',')[0],
+                    "petrol_price" : data['petrol_price'].split(',')[0],
+                    "disel_price" : data['disel_price'].split(',')[0],
+                    "lpg_price" : data['lpg_price'].split(',')[0],
+                    "no_of_drivers" : data['no_of_drivers'].split(',')[0],
+                    "drive_time_per_drver" : data['drive_time_per_drver'].split(',')[0],
+                    "lpg_cars_no" : data['lpg_cars_no'].split(',')[0],
+                    "petrol_cars_no" : data['petrol_cars_no'].split(',')[0],
+                    "disel_cars_no" : data['disel_cars_no'].split(',')[0],
+                    "avg_maint_cost" : data['avg_maint_cost'].split(',')[0],
+                    "avg_petrol_milage" : data['avg_petrol_milage'].split(',')[0],
+                    "avg_disel_milege" : data['avg_disel_milege'].split(',')[0],
+                    "avg_lpg_milege" : data['avg_lpg_milege'].split(',')[0]
+                }
+                print(simdata)
+                sim = SimulationTask(simdata)
+                db_session.add(sim)
+                print ('added')
+                db_session.commit()
+                print("Simulation Task is done!")
+                return jsonify(response=200,Status="success")
+            except Exception as e:
+                print(e)
+                return jsonify(response=400, Statis="exception")
+        else:
+            return jsonify(response=400,Status="Task In Progress Rejecting Input")
+
 
     @app.route('/test')
     def test():
-        tasks = db.session.query(Task).all()
-        return u"<br>".join([u"{0}: {1}".format(task.name, task.status) for task in tasks])
+        tasks = db_session.query(SimulationTask).all()
+        return u"<br>".join([u"{0}: {1}".format(task.id, task.status) for task in tasks])
 
 
 
